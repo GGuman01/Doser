45e85e34f75686f90fad5b708fa5e4116eed9f30
Guman Garayev <garaevguman@gmail.com>
2024-11-15 04:55:50 +0400

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -114,7 +114,7 @@ REST_FRAMEWORK = {
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
-
+###################################################
 LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "UTC"
